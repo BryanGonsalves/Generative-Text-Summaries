95c2555687a1cdc2ff22cbb20c0ec289a423e365
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-18 12:06:14 +0400

    added a text box with input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-<h1>Summerizer</h1>
-<p>Welcome to Summarizer! Insert your text below to get summarized.</p>
-
+  <h1>Summarizer</h1>
+  <p>Welcome to Summarizer! Insert your text below to get it summarized.</p>
+  <form id="ruleForm">
+      <textarea v-model="input" form="ruleForm" rows="20" cols="40" placeholder="Provide the text here"></textarea>
+      <br/>
+      <button style="margin-top: 0.5rem;" v-on:click.prevent="clear">Clear</button>
+  </form>
 </template>
 
 <script>
-
 export default {
   name: 'App',
+  data() {
+      return {
+          input: ""
+      }
+  },
+  methods: {
+      clear() {
+          this.input = ""
+      },
+  }
 }
 </script>
