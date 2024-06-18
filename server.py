b4401c8e838565e1e959d8470019cd7c04bc51f6
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-18 13:42:15 +0400

    added persona feature

--- server.py
+++ server.py
@@ -6,12 +6,18 @@ from openai import OpenAI
 app = Flask(__name__)
 client = OpenAI(api_key="YOUR_SECRET_KEY_API")
 @app.route('/summary', methods=['POST'])
+@app.route('/summary', methods=['POST'])
 def get_summary():
     text = request.get_json()['text']
+    persona = 'High-school English teacher'
     prompt = f"Create the summary of the following text: {text}"
     response = client.chat.completions.create(
         model='gpt-4',
         messages=[
+            {
+                'role': 'system',
+                'content': f'You are a {persona}'
+            },
             {
                 'role': 'user',
                 'content': prompt
@@ -19,7 +25,7 @@ def get_summary():
         ]
     )
     return response.choices[0].message.content
-    if __name__ =="__main__":
-        app.run()
+if __name__ =="__main__":
+    app.run()
         
         
