e13909e8c45c3f9a17407623db173427b16056c4
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-18 12:59:36 +0400

    update backward url

--- server.py
+++ server.py
@@ -2,5 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/summary',methods=['POST'])
+def getSummary():
+    return request.get_json()['text']
+
 if __name__ == "__main__":
     app.run()
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,52 @@
   <h1>Summarizer</h1>
   <p>Welcome to Summarizer! Insert your text below to get it summarized.</p>
   <form id="ruleForm">
-      <textarea v-model="input" form="ruleForm" rows="20" cols="40" placeholder="Provide the text here"></textarea>
-      <br/>
-      <button style="margin: 0.5rem 0.5rem 0 0;" v-on:click.prevent="createSummary">Create Summary</button>
-      <button v-on:click.prevent="clear">Clear</button>
+    <textarea
+      v-model="input"
+      form="ruleForm"
+      rows="15"
+      cols="40"
+      placeholder="Provide the text here"
+    ></textarea>
+    <br />
+    <button style="margin: 1rem 1rem 0 0" v-on:click.prevent="createSummary">
+      Create Summary
+    </button>
+    <button v-on:click.prevent="clear">Clear</button>
+    <p>{{ output }}</p>
   </form>
-  <p>{{ output }}</p>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+
 export default {
-  name: 'App',
+  name: "App",
   data() {
-      return {
-          input: "",
-          output: ""
-      }
+    return {
+      input: "",
+      output: "",
+    };
   },
   methods: {
-      clear() {
-          this.input = "",
-          this.output = ""
-      },
-      async createSummary() {
-          try {
-              let response = await axios.post("http://127.0.0.1:5000/", {'text' : this.input})
-              this.output = response.data
-          } catch (error) {
-              this.output = error
-          }
+    clear() {
+      this.input = "";
+      this.output = "";
+    },
+    async createSummary() {
+      let data = {
+        text: this.input,
+      };
+      try {
+        let response = await axios.post(
+          window.location.href + "summary",
+          data
+        );
+        this.output = response.data;
+      } catch (error) {
+        this.output = error;
       }
-  }
-}
+    },
+  },
+};
 </script>
