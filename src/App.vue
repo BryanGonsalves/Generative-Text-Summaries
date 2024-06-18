4699da92cbc3c59b148ee2ce1fc113ec342e8a23
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-18 12:13:17 +0400

    Add createSummary method to App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
   <form id="ruleForm">
       <textarea v-model="input" form="ruleForm" rows="20" cols="40" placeholder="Provide the text here"></textarea>
       <br/>
-      <button style="margin-top: 0.5rem;" v-on:click.prevent="clear">Clear</button>
+      <button style="margin: 0.5rem 0.5rem 0 0;" v-on:click.prevent="createSummary">Create Summary</button>
+      <button v-on:click.prevent="clear">Clear</button>
   </form>
+  <p>{{ output }}</p>
 </template>
 
 <script>
@@ -13,13 +15,18 @@ export default {
   name: 'App',
   data() {
       return {
-          input: ""
+          input: "",
+          output: ""
       }
   },
   methods: {
       clear() {
-          this.input = ""
+          this.input = "",
+          this.output = ""
       },
+      createSummary() {
+          this.output = this.input
+      }
   }
 }
 </script>
