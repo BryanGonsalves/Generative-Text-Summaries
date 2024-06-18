c11bb500b33ff8973d77c4c210b2883651986ca8
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-18 13:36:28 +0400

    updated /summary method

--- server.py
+++ server.py
@@ -5,20 +5,20 @@ from openai import OpenAI
 
 app = Flask(__name__)
 client = OpenAI(api_key="YOUR_SECRET_KEY_API")
-@app.route('/summary',methods=['POST'])
+@app.route('/summary', methods=['POST'])
 def get_summary():
-    prompt = request.get_json()['text']
+    text = request.get_json()['text']
+    prompt = f"Create the summary of the following text: {text}"
     response = client.chat.completions.create(
-        model = 'gpt-4',
+        model='gpt-4',
         messages=[
             {
                 'role': 'user',
-                'content':prompt
+                'content': prompt
             }
         ]
     )
     return response.choices[0].message.content
-
     if __name__ =="__main__":
         app.run()
         
